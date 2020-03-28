0c39640a4ff3454da6c74914e78b85c4c1f468ab
p0bailey <phillip@bailey.st>
2020-03-28 17:43:36 +0000

    tf12

--- sg_gen/main.tf
+++ sg_gen/main.tf
@@ -1,20 +1,20 @@
 resource "aws_security_group" "main_security_group" {
-  name   = "${var.security_group_name}"
-  vpc_id = "${var.vpc_id}"
+  name   = var.security_group_name
+  vpc_id = var.vpc_id
 
-  tags {
-    Name        = "${var.name}"
-    Environment = "${var.environment}"
+  tags = {
+    Name        = var.name
+    Environment = var.environment
     terraform   = "true"
   }
 
   ingress {
-    from_port       = "${var.from_port}"
-    to_port         = "${var.to_port}"
-    protocol        = "${var.protocol}"
-    cidr_blocks     = ["${var.allowed_cidr_blocks}"]
-    security_groups = ["${var.allowed_security_groups}"]
-    self            = "${var.self}"
+    from_port       = var.from_port
+    to_port         = var.to_port
+    protocol        = var.protocol
+    cidr_blocks     = var.allowed_cidr_blocks
+    security_groups = var.allowed_security_groups
+    self            = var.self
   }
   # Allow ICMP Echo Request and Echo Reply (ping) - http://shouldiblockicmp.com/
   ingress {
